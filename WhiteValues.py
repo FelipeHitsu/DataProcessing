e9e4b78ba156a888a94ae043116512e638371610
Felipe Silva "Hitsu <felipe.silva.hts@gmail.com>
2019-11-29 16:16:37 -0300

    training

--- WhiteValues.py
+++ WhiteValues.py
@@ -17,3 +17,20 @@ np.random.seed(25)
 df = DataFrame(np.random.rand(36).reshape((6,6)))
 print(df)
 print('')
+
+df.loc[3:5,0] = white_value
+df.loc[1:4,5] = white_value
+print(df)
+print('')
+
+df_process = df.fillna(0)
+print(df_process)
+print('')
+
+dc = {0: 0.1,5: 1.25}
+df_process = df.fillna(dc)
+print(df_process)
+print('')
+
+#count and remove
+
